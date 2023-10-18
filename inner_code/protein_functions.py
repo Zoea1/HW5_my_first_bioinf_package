28ac7a2c15196ef4c1794d647c544551344d76fd
Zoea1 <143959084+Zoea1@users.noreply.github.com>
2023-10-18 21:58:31 +0700

    Fix errors in protein_functions.py

--- inner_code/protein_functions.py
+++ inner_code/protein_functions.py
@@ -1,20 +1,20 @@
-def count_aa_length (arg: str) -> int:  
+def count_aa_length(arg: str) -> int:  
     """ 
-    Counts the length of the sequence\n
-    Arguments:\n
-    arg (str) - the sequence, which length should be counted\n
-    Return:\n
+    Counts the length of the sequence
+    Arguments:
+    arg (str) - the sequence, which length should be counted
+    Return:
     int - the result of the count
     """
-    return len (arg)
+    return len(arg)
 
 
-def count_nucl_length (arg: str) -> int: 
+def count_nucl_length(arg: str) -> int: 
     """
-    Counts the length of the nucleotide sequence that codes the inputted aminoacid sequence\n
-    Arguments:\n
-    arg (str) - the sequence, which coding nucleotide sequence length should be counted\n
-    Return:\n
+    Counts the length of the nucleotide sequence that codes the inputted aminoacid sequence
+    Arguments:
+    arg (str) - the sequence, which coding nucleotide sequence length should be counted
+    Return:
     int - the result of the count
     """
-    return len (arg) * 3
+    return len(arg) * 3
